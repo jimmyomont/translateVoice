df8fc58be7c41f5bcd7087010f1470b0ed18c0fb
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-08-11 08:35:11 +0200

    testing voice navigateur mobile

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 
-function lireTexte() {
-    const texte = document.getElementById('resultatItalien').value;
-    const speech = new SpeechSynthesisUtterance(texte);
-    window.speechSynthesis.speak(speech);
-}
+
 // ----------------------------------------------Traducteur-----------------------------------------------
 
 function traduire() {
@@ -17,23 +13,59 @@ function traduire() {
         .then(response => response.json())
         .then(data => {
             const resultat = data.responseData.translatedText;
-            document.getElementById('resultatItalien').textContent = resultat;
+            document.getElementById('resultatTraduit').textContent = resultat;
         })
         .catch(error => console.error('Erreur :', error));
 }
 
-function lireTexte() {
-    const texte = document.getElementById('resultatItalien').value;
-    const lang = document.getElementById('voiceSelect').value;
-    const speech = new SpeechSynthesisUtterance(texte);
-    speech.lang = lang;
+function reinitialiser() {
+    document.getElementById('texteFrancais').value = '';
+    document.getElementById('resultatTraduit').value = '';
+}
+//-----------------------Voix navigateur-------------------
+
+let speech = new SpeechSynthesisUtterance();
+let voix = [];
+let voixNavigateur = document.getElementById("voixNavigateur");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voix = window.speechSynthesis.getVoices();
+    speech.voix = voix[0];
+    voix.forEach((voix, idx) => (voixNavigateur.options[idx + 1] = new Option(voix.name, idx)));
+};
+
+voixNavigateur.addEventListener("change", () => {
+    speech.voix = voix[voixNavigateur.value];
+});
+
+function lireTexteAvecVoixNavigateur() {
+    const texte = document.getElementById('resultatTraduit').value;
+    speech.text = texte;
     window.speechSynthesis.speak(speech);
 }
 
-function reinitialiser() {
-    document.getElementById('texteFrancais').value = '';
-    document.getElementById('resultatItalien').value = '';
+// ... Vos fonctions existantes ...
+
+function lireTexteAvecVoixNavigateur() {
+    const texte = document.getElementById('resultatTraduit').value;
+    speech.text = texte;
+
+    const lang = document.getElementById('voixNavigateur').value;
+
+    if (speech.voix) {
+        speech.lang = speech.voix.lang;
+    } else if (lang) {
+        speech.lang = lang;
+    } else {
+        alert("Veuillez sélectionner une voix du navigateur ou une langue cible.");
+        return;
+    }
+
+    window.speechSynthesis.speak(speech);
 }
+
+// ... Le reste de votre code ...
+
 
 //---------------------Horloge-------------------------------
 
